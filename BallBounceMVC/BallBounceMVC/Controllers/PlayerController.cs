1fd84a11ae20f3d9f93c6aeca00c266159f2f68d
Daniel Lee <daniel_lee_limerick@yahoo.com>
2012-04-04 00:12:50 +0200

    Added WP7 project linked to original project and added touch support.

--- BallBounceMVC/BallBounceMVC/Controllers/PlayerController.cs
+++ BallBounceMVC/BallBounceMVC/Controllers/PlayerController.cs
@@ -1,5 +1,10 @@
+using System;
 using BallBounceMVC.Models;
 using Microsoft.Xna.Framework.Input;
+
+#if WINDOWS_PHONE
+using Microsoft.Xna.Framework.Input.Touch;
+#endif
 
 namespace BallBounceMVC.Controllers
 {
@@ -11,19 +16,30 @@ namespace BallBounceMVC.Controllers
         public PlayerController(PlayerModel playerModel)
         {
             _playerModel = playerModel;
+#if WINDOWS_PHONE
+            TouchPanel.EnabledGestures = GestureType.HorizontalDrag;
+#else
             _previousMouseState = Mouse.GetState();
+#endif
         }
 
         public override void Control(float elapsedSeconds)
         {
+        #if WINDOWS_PHONE 
+            if(TouchPanel.IsGestureAvailable) { 
+            var gesture = TouchPanel.ReadGesture(); 
+            var delta = gesture.Delta.X;
+            _playerModel.Update(delta); 
+            } 
+        #else 
             MouseState currentMouseState = Mouse.GetState();
             if (currentMouseState != _previousMouseState)
             {
                 float xDifference = currentMouseState.X - _previousMouseState.X;
                 _playerModel.Update(xDifference);
             }
-
             _previousMouseState = currentMouseState;
+        #endif
         }
     }
 }
